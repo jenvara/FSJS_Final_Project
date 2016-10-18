9f58780fef8f32b725531a00c3033cc6375f459c
Jennifer Varahala <Jennifer Varahala>
2016-10-18 13:25:36 -0400

    Initial file creation and planning of project

---
+++ app.js
@@ -0,0 +1,12 @@
+//Problem: I need a simple to do list to know where I am at with FSJS course syllabus without having to scroll through a syllabus
+//Create a syllabus page
+	//Build App to-do list for course syllabus
+		//Weekly Assignments
+		//Checkbox for completion
+		//Add button to add extra assignments (add approximate time to complete if possible)
+//If I have extra time
+	//Timer
+		//Countdown timer in hours
+		//Subtracts time from each assignment as it's checked completed
+
+
---
+++ code_challenge.js
@@ -0,0 +1,6 @@
+//Problem: I need a simple to do list to know where I am at with FSJS code challenges
+//Create a code challenge page
+	//Build App to-do list fo code challenges
+		//Weekly Assignments
+		//Checkbox for completion
+		//Add button to add extra assignments
